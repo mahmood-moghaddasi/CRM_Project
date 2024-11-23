ce750307267e8d18247481e73ef3f300d30dd2f3
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-11-23 20:57:37 +0330

    add customer page completed

--- components/templates/AddCustomerPage.js
+++ components/templates/AddCustomerPage.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Form from "../module/Form";
+import { useRouter } from "next/router";
 
 function AddCustomerPage() {
   const [form, setForm] = useState({
@@ -11,9 +13,45 @@ function AddCustomerPage() {
     date: "",
     products: [],
   });
+  const router = useRouter();
+
+  const cancelHandler = () => {
+    setForm({
+      name: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      address: "",
+      postalCode: "",
+      date: "",
+      products: [],
+    });
+    router.push("/");
+  };
+  const saveHandler = async () => {
+    const res = await fetch("/api/customer", {
+      method: "POST",
+      body: JSON.stringify({ data: form }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    if (data.status === "succes") router.push("/");
+  };
   return (
     <div className="customer-page">
       <h4>Add New Customer</h4>
+      <Form form={form} setForm={setForm} />
+      <div className="customer-page__buttons">
+        <button className="first" onClick={cancelHandler}>
+          Cancel
+        </button>
+        <button className="second" onClick={saveHandler}>
+          Save
+        </button>
+      </div>
     </div>
   );
 }

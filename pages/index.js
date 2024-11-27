2bd461c7398c2de843121bb259e959d78cf9229b
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-11-27 20:59:03 +0330

    creating homePage cards

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 import connectDB from "../utils/ConnectDB";
+import Customer from "../models/Customer";
+import HomePage from "../components/templates/HomePage";
+export default function Index({ customers }) {
+  console.log(customers);
+  return <HomePage customers={customers} />;
+}
 
-export default function Home() {
-  return <div></div>;
+export async function getServerSideProps(params) {
+  try {
+    await connectDB();
+    const customers = await Customer.find();
+    return {
+      props: { customers: JSON.parse(JSON.stringify(customers)) },
+    };
+  } catch (error) {
+    return { notFound: true };
+  }
 }
